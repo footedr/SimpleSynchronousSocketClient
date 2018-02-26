6286e45ff9edcc6baa9abec357c8e88fcf55a230
Ryan Foote <footedr@gmail.com>
2018-02-26 14:33:21 -0500

    Created .NET Core console app with simple synchronous socket client.

---
+++ src/ClientApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClientApp
+{
+	internal class Program
+	{
+		private const string YesResponse = "Y";
+		private const string NoResponse = "N";
+
+		private static void Main(string[] args)
+		{
+			DisplayPrompts();
+
+			var response = DisplayConfirmPrompt();
+
+			if (response.Equals(YesResponse, StringComparison.CurrentCultureIgnoreCase))
+				DisplayPrompts();
+			else if (!response.Equals(NoResponse, StringComparison.CurrentCultureIgnoreCase))
+			{
+				Console.WriteLine("Invalid response.");
+				DisplayConfirmPrompt();
+			}
+		}
+
+		private static void DisplayPrompts()
+		{
+			Console.WriteLine("Host:");
+			var host = Console.ReadLine();
+
+			Console.WriteLine("Port:");
+			var port = Console.ReadLine();
+
+			Console.WriteLine("Message:");
+			var message = Console.ReadLine();
+
+			SynchonousSocketClient.Send(host, Convert.ToInt32(port), message);
+		}
+
+		private static string DisplayConfirmPrompt()
+		{
+			Console.WriteLine("Send another message? (Y/N):");
+			return Console.ReadLine() ?? NoResponse;
+		}
+	}
+}
---
+++ src/ClientApp/SynchonousSocketClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientApp
+{
+	public class SynchonousSocketClient
+	{
+		public static void Send(string host, int port, string message)
+		{
+			var incomingData = new byte[1024];
+
+			try
+			{
+				var ipHostInfo = Dns.GetHostEntry(host);
+				var ipAddress = ipHostInfo.AddressList[0];
+				var remoteEndpoint = new IPEndPoint(ipAddress, port);
+
+				var sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+				sender.Connect(remoteEndpoint);
+
+				Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+				var messageData = Encoding.ASCII.GetBytes(message);
+
+				_ = sender.Send(messageData);
+
+				var bytesReceived = sender.Receive(incomingData);
+				Console.WriteLine($"Received: {Encoding.ASCII.GetString(incomingData, 0, bytesReceived)}");
+
+				sender.Shutdown(SocketShutdown.Both);
+				sender.Close();
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.Message);
+			}
+		}
+	}
+}
